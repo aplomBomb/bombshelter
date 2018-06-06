6fb7eedf369333dd2471fb648a4b510460442510
Kacie <kacie.miller.7@gmail.com>
2018-06-06 19:59:32 -0400

    nav slide integration

---
+++ script.js
@@ -0,0 +1,21 @@
+function closeNavLeft() {
+    document.getElementById("twitchBladeOpen").style.width ="0vw";
+}
+function openNavLeft() {
+    document.getElementById("twitchBladeOpen").style.position = "fixed";
+    document.getElementById("twitchBladeOpen").style.left = "0";
+    document.getElementById("twitchBladeOpen").style.width = "30vw";
+}
+
+function closeNavRight() {
+    document.getElementById("youtubeBladeOpen").style.width ="0vw";
+}
+function openNavRight() {
+    document.getElementById("youtubeBladeOpen").style.position = "fixed";
+    document.getElementById("youtubeBladeOpen").style.right = "0";
+    document.getElementById("youtubeBladeOpen").style.width = "30vw";
+}
+
+
+
+
--- scripts.js
+++
@@ -1,4 +0,0 @@
-var input1 = prompt("Please enter 1st number");
-while (input1 = isNaN(false); {
-	alert("Please enter a whole number");
-}
