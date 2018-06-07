11e6e710a4353bb7d1d29777f36ac40a6af0cab0
Kacie <kacie.miller.7@gmail.com>
2018-06-07 11:17:55 -0400

    responsive work on embeds

--- navControl.js
+++ navControl.js
@@ -2,14 +2,15 @@ function closeNavLeft() {
     document.getElementById("twitchBladeOpen").style.width ="0vw";
 }
 function openNavLeft() {
+    document.getElementById("youtubeBladeOpen").style.width ="0vw";
     document.getElementById("twitchBladeOpen").classList.remove("hidden");
     document.getElementById("twitchBladeOpen").style.position = "fixed";
     document.getElementById("twitchBladeOpen").style.left = "0";
     if (window.matchMedia("(max-width: 1800px)").matches) {
-        document.getElementById("twitchBladeOpen").style.width = "48vw";
+        document.getElementById("twitchBladeOpen").style.width = "85vw";
         /* the viewport is at most 1800 pixels wide */
       } else {
-        document.getElementById("twitchBladeOpen").style.width = "30vw";
+        document.getElementById("twitchBladeOpen").style.width = "100vw";
         /* the viewport is more than 1800 pixels wide */
       }
     
@@ -19,14 +20,15 @@ function closeNavRight() {
     document.getElementById("youtubeBladeOpen").style.width ="0vw";
 }
 function openNavRight() {
+    document.getElementById("twitchBladeOpen").style.width ="0vw";
     document.getElementById("youtubeBladeOpen").classList.remove("hidden");
     document.getElementById("youtubeBladeOpen").style.position = "fixed";
     document.getElementById("youtubeBladeOpen").style.right = "0";
     if (window.matchMedia("(max-width: 1800px)").matches) {
-        document.getElementById("youtubeBladeOpen").style.width = "48vw";
+        document.getElementById("youtubeBladeOpen").style.width = "85vw";
         /* the viewport is at most 1800 pixels wide */
       } else {
-        document.getElementById("youtubeBladeOpen").style.width = "30vw";
+        document.getElementById("youtubeBladeOpen").style.width = "100vw";
         /* the viewport is more than 1800 pixels wide */
       }
 }
