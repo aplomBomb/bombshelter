6bb866a46a8d31720fb75a1fb686d0d65d9bb4bb
bomb <seizethesteez@gmail.com>
2018-06-07 22:44:58 -0400

    nav close optimized

--- navControl.js
+++ navControl.js
@@ -1,36 +1,29 @@
 function closeNavLeft() {
+    document.getElementById("twitchIconClose").style.display = "none";
     document.getElementById("twitchBladeOpen").style.width ="0vw";
 }
 function openNavLeft() {
     document.getElementById("youtubeBladeOpen").style.width ="0vw";
-    document.getElementById("twitchBladeOpen").classList.remove("hidden");
     document.getElementById("twitchBladeOpen").style.position = "fixed";
     document.getElementById("twitchBladeOpen").style.left = "0";
-    if (window.matchMedia("(max-width: 1800px)").matches) {
-        document.getElementById("twitchBladeOpen").style.width = "85vw";
-        /* the viewport is at most 1800 pixels wide */
-      } else {
-        document.getElementById("twitchBladeOpen").style.width = "100vw";
-        /* the viewport is more than 1800 pixels wide */
-      }
-    
+    document.getElementById("twitchBladeOpen").style.width = "85vw";
+    document.getElementById("twitchIconClose").style.position = "fixed";   
+    document.getElementById("twitchIconClose").style.left = "-1em";
+    document.getElementById("twitchIconClose").style.display = "block";    
 }
 
 function closeNavRight() {
+    document.getElementById("youtubeIconClose").style.display = "none";
     document.getElementById("youtubeBladeOpen").style.width ="0vw";
 }
 function openNavRight() {
     document.getElementById("twitchBladeOpen").style.width ="0vw";
-    document.getElementById("youtubeBladeOpen").classList.remove("hidden");
     document.getElementById("youtubeBladeOpen").style.position = "fixed";
     document.getElementById("youtubeBladeOpen").style.right = "0";
-    if (window.matchMedia("(max-width: 1800px)").matches) {
-        document.getElementById("youtubeBladeOpen").style.width = "85vw";
-        /* the viewport is at most 1800 pixels wide */
-      } else {
-        document.getElementById("youtubeBladeOpen").style.width = "100vw";
-        /* the viewport is more than 1800 pixels wide */
-      }
+    document.getElementById("youtubeBladeOpen").style.width = "85vw";
+    document.getElementById("youtubeIconClose").style.position = "fixed";   
+    document.getElementById("youtubeIconClose").style.right = "-.75em";
+    document.getElementById("youtubeIconClose").style.display = "block";     
 }
 
 
